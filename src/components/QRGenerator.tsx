fc8aca345761b2df8ff8f09a458fc491ddd633bc
Mohammad Qudah <77822606+QudahM@users.noreply.github.com>
2025-06-28 00:36:48 -0400

    Updated QRGenerator.tsx

--- src/components/QRGenerator.tsx
+++ src/components/QRGenerator.tsx
@@ -25,7 +25,7 @@ const QRGenerator: React.FC = () => {
         margin: 2,
         color: {
           dark: '#021526',
-          light: '#F5F5F5',
+          light: '#FAF6E9',
         },
         errorCorrectionLevel: 'H',
       });
